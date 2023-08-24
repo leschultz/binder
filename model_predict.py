7c0187d54c943ef004e2ccf4828a66bb0edf28ba
leschultz <laneenriqueschultz@gmail.com>
2023-08-24 13:39:50 -0500

    second

--- model_predict.py
+++ model_predict.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import dill
 
-df = pd.read_csv('/mnt/test.csv')
+df = pd.read_csv('X.csv')
 
 with open('model.dill', 'rb') as handle:
     model = dill.load(handle)
 
 df = model.predict(df.values)
-df.to_csv('/mnt/prediction.csv', index=False)
+print(df)
--- user_predict.py
+++
@@ -1,9 +0,0 @@
-from madml.hosting.docker import predict
-import numpy as np
-
-container_name = 'leschultz/cmg:diffusion'
-
-X = np.loadtxt('X.csv', delimiter=',')
-
-y = predict(X, container_name)
-print(y)
